70fbbd8936e949a5247cf22dfae91ba05a22589e
Prashant Kumar Gautam <PrashantG@talentica-all.com>
2022-01-25 00:36:45 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import csv
+import re
+import PyPDF2
+
+pdfReader = PyPDF2.PdfFileReader('INPUT/Sample.pdf')
+
+pageNum = pdfReader.getNumPages()
+
+String= "Pune"
+
+for i in range(0,pageNum):
+    PageObj = pdfReader.getPage(i)
+    #print("this is page " + str(i))
+    Text = PageObj.extractText()
+    #print(Text)
+    ResSearch = re.search(String, Text)
+    print(ResSearch)
+
+#print(pageObj.extractText())
+
+#a=pageObj.ext
+
