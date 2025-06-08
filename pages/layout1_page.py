cad018ff91b9eaf29f968814be6ca9a279716e72
bragaji <psprasha50@gmail.com>
2025-06-07 23:19:19 -0400

    tool-tip fix and, Explicit wait

--- pages/layout1_page.py
+++ pages/layout1_page.py
@@ -1,5 +1,8 @@
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class Layout1Page:
     def __init__(self,driver):
@@ -7,10 +10,13 @@ class Layout1Page:
         self.alert_button=(By.XPATH,'//button[text()=" Your Sample Alert Button!"]')
         self.alert_text=(By.ID,'demo')
         self.tool_tip=(By.XPATH,"//div[@class='tooltip']")
-        self.tool_tip_text=(By.CLASS_NAME,'tooltip-text')
+        self.tool_tip_text=(By.CLASS_NAME,'tooltiptext')
 
     def click_alert_button(self):
-        self.driver.find_element(*self.alert_button).click()
+        WebDriverWait(self.driver,10).until(
+            EC.element_to_be_clickable(self.alert_button)
+        ).click()
+        #self.driver.find_element(*self.alert_button).click()
 
     def accept_alert(self):
         alert = self.driver.switch_to.alert
@@ -22,8 +28,12 @@ class Layout1Page:
         print(element.text)
 
     def get_tool_tip(self):
-        tooltip_element = self.driver.find_element(*self.tool_tip)
+        tooltip_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.tool_tip)
+        )
         ActionChains(self.driver).move_to_element(tooltip_element).perform()
-        tooltip_text = self.driver.find_element(By.CLASS_NAME, 'tooltiptext').text
+
+        tooltip_text = self.driver.find_element(*self.tool_tip_text).text
         print("Tooltip Text:", tooltip_text)
         self.tool_tip_text = tooltip_text
+
